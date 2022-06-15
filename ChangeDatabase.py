6e44ea7b3e0dcb98730930836665dbae72f21458
Melvin <melvinvandriel00@gmail.com>
2022-06-15 13:55:03 +0200

    Read database function is not hardcoded anymore

--- ChangeDatabase.py
+++ ChangeDatabase.py
@@ -4,12 +4,18 @@ print("**************")
 connection = sqlite3.connect('BitebyBite.db')
 c = connection.cursor()
 
-
 def ReadDatabase():
-    print("Tables:\ningredients\nuser_status\nuser_password\n\n")
+    c.execute('''SELECT name FROM sqlite_schema''')
+    listoftables = c.fetchall()
+    print("Tables:")
+    for i in listoftables:
+        print(i[0])
+    #print("Tables:\ningredients\nuser_status\nuser_password\ncart\n\n")
     table = input("Enter your table: ")
     sql = '''SELECT * FROM '''
-    for row in c.execute(sql+table):
+    c.execute(sql+table)
+    selectedtable = c.fetchall()
+    for row in selectedtable:
         print(row)
 
 def AddIngredient():
@@ -19,43 +25,40 @@ def AddIngredient():
     newproduct = (product, costnm)
     c.execute('''INSERT INTO ingredients values (?,?)", newproduct''')
 
+def AddToCart():
+    print("Hi")
+
+
+def RemoveFromCart():
+    print("Oh no")
+
+
 def ChangePrice():
+    #These are our products:
     c.execute('''SELECT * FROM ingredients''')
     results = c.fetchall()
-    #These are our products:
     print(results)
-    productlist = []
-    for i in results:
-        product = i[0]
-        productlist.append(product)
-    print(productlist)
 
     #Search for your chosen product
-    productselect = input("SELECT a product: ")
+    productselect = input("Select a product: ")
     sql1 = '''SELECT * FROM ingredients WHERE name=:c'''
     sql2 = {"c": productselect}
     c.execute(sql1, sql2)
     Productsearch = c.fetchall() # our tuple [(Aspergus, 2.3)]
 
+    #show current price
     currentprice = Productsearch[0][1]
     print(f"The current price of this product is: {currentprice}")
-    print(Productsearch)
     
     #replace price
     chosenprice = input("Choose a new price: ")
-
-    #replace in table
-    c.execute('''SELECT * FROM ingredients''')
-    ingredient = c.fetchall()
-    print(ingredient)
     
+    #UPDATE the table "ingredients"
     sql20 = '''UPDATE ingredients SET price = '''
     sql21 = ''' WHERE name="'''
     sql22 = '''"'''
     c.execute(sql20 + chosenprice + sql21 + productselect + sql22)
 
-ChangePrice()
-print("You have changed the price")
 ReadDatabase()
 
 
