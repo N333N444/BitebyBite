cdb4f42a4f54f79ec690f71aef8f1db874266289
HuubSM <huub.statiusmuller@gmail.com>
2022-06-22 21:09:57 +0200

    namen en prijzen van ingredienten in database veranderd

--- ChangeDatabase.py
+++ ChangeDatabase.py
@@ -2,35 +2,42 @@ import sqlite3
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-counterlist = ["0","0","0","0","0","0","0","0"]
-mbcounterlist = ["1","1","1","1","1","1","1","1"]
+counterlist = ["0", "0", "0", "0", "0", "0", "0", "0"]
+mbcounterlist = ["1", "1", "1", "1", "1", "1", "1", "1"]
 counterVal = 0
+
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/Product.html/')
 def productpage():
-    return render_template('Product.html', counters = counterlist)
+    return render_template('Product.html', counters=counterlist)
+
 
 @app.route('/cart/')
 def cart():
     totalprice = CalcFinalPrice()
     productlist = ReadCartInfo()
-    return render_template('Shoppingcart.html', productlist = productlist, total = totalprice)
+    return render_template('Shoppingcart.html', productlist=productlist, total=totalprice)
+
 
 @app.route('/About_us.html/')
 def About():
     return render_template('About_us.html')
 
+
 @app.route('/Dishes.html/')
 def Dishes():
     return render_template('Dishes.html')
 
+
 @app.route('/VeganKap.html/')
 def vegankapdish():
-    return render_template('VeganKap.html', mbcounter = mbcounterlist)
+    return render_template('VeganKap.html', mbcounter=mbcounterlist)
+
 
 @app.route('/omvk/', methods=['POST'])
 def omvk():
@@ -39,9 +46,11 @@ def omvk():
     AddToCart("MealboxVeganKap", quantity)
     return render_template("Shoppingcart.html")
 
+
 print("**************")
 connection = sqlite3.connect('BitebyBite.db', check_same_thread=False)
 c = connection.cursor()
+
 
 def ReadCartInfo():
     table = "cart"
@@ -49,15 +58,16 @@ def ReadCartInfo():
     c.execute(sql+table)
     selectedtable = c.fetchall()
     for i in range(0, len(selectedtable)):
-        x = list(selectedtable[i]) #make (asp, 1.0,0) a list
-        z = str(x[0]) # make asp a sting
+        x = list(selectedtable[i])  # make (asp, 1.0,0) a list
+        z = str(x[0])  # make asp a sting
         y = z.replace("_", " ")
-        print(y) # replace string
-        x[0] = y # replace asp with correct word in list
+        print(y)  # replace string
+        x[0] = y  # replace asp with correct word in list
         print(x)
         selectedtable[i] = tuple(x)
     productlist = selectedtable
     return productlist
+
 
 def ReadDatabase():
     c.execute('''SELECT name FROM sqlite_schema''')
@@ -65,13 +75,14 @@ def ReadDatabase():
     print("Tables:")
     for i in listoftables:
         print(i[0])
-    #print("Tables:\ningredients\nuser_status\nuser_password\ncart\n\n")
+    # print("Tables:\ningredients\nuser_status\nuser_password\ncart\n\n")
     table = input("Enter your table: ")
     sql = '''SELECT * FROM '''
     c.execute(sql+table)
     selectedtable = c.fetchall()
     for row in selectedtable:
         print(row)
+
 
 def AddIngredient():
     product = input("Enter product name: ")
@@ -80,14 +91,15 @@ def AddIngredient():
     newproduct = (product, costnm)
     c.execute('''INSERT INTO ingredients values (?,?)", newproduct''')
 
+
 def AddToCart(productname, Currentquantity):
-    #Search for your chosen product
+    # Search for your chosen product
     sql1 = '''SELECT * FROM ingredients WHERE name=:c'''
     sql2 = {"c": productname}
     c.execute(sql1, sql2)
     Productsearch = c.fetchall()
     productname = str(Productsearch[0][0])
-    
+
     productprice = Currentquantity * float(Productsearch[0][1])
     cartlist = (productname, productprice, Currentquantity)
     # check if the product is in the cart
@@ -105,8 +117,9 @@ def AddToCart(productname, Currentquantity):
         sql4 = '''UPDATE cart SET price = '''
         c.execute(sql4 + str(productprice) + sql2 + productname + sql3)
 
+
 def RemoveFromCart(productname, Currentquantity):
-    #Search for your chosen product
+    # Search for your chosen product
     sql1 = '''SELECT * FROM ingredients WHERE name=:c'''
     sql2 = {"c": productname}
     c.execute(sql1, sql2)
@@ -120,31 +133,33 @@ def RemoveFromCart(productname, Currentquantity):
         sql3 = '''"'''
         c.execute(sql1 + str(Currentquantity) + sql2 + productname + sql3)
 
+
 def ChangePrice():
-    #These are our products:
+    # These are our products:
     c.execute('''SELECT * FROM ingredients''')
     results = c.fetchall()
     print(results)
 
-    #Search for your chosen product
+    # Search for your chosen product
     productselect = input("Select a product: ")
     sql1 = '''SELECT * FROM ingredients WHERE name=:c'''
     sql2 = {"c": productselect}
     c.execute(sql1, sql2)
-    Productsearch = c.fetchall() # our tuple [(Aspergus, 2.3)]
+    Productsearch = c.fetchall()  # our tuple [(Aspergus, 2.3)]
 
-    #show current price
+    # show current price
     currentprice = Productsearch[0][1]
     print(f"The current price of this product is: {currentprice}")
-    
-    #replace price
+
+    # replace price
     chosenprice = input("Choose a new price: ")
-    
-    #UPDATE the table "ingredients"
+
+    # UPDATE the table "ingredients"
     sql20 = '''UPDATE ingredients SET price = '''
     sql21 = ''' WHERE name="'''
     sql22 = '''"'''
     c.execute(sql20 + chosenprice + sql21 + productselect + sql22)
+
 
 def CalcFinalPrice():
     c.execute('''SELECT * FROM cart''')
@@ -152,28 +167,30 @@ def CalcFinalPrice():
     print(results)
     totalprice = 0
     for i in results:
-        totalprice = float(totalprice + i[1])   
+        totalprice = float(totalprice + i[1])
     totalprice = "%.2f" % totalprice
     return totalprice
 
-def pluscount(productname,i):
+
+def pluscount(productname, i):
     sql1 = '''SELECT * FROM cart WHERE name=:c'''
     sql2 = {"c": productname}
     c.execute(sql1, sql2)
-    Productselect = c.fetchall() # our tuple [(Aspergus, 2.3)]
+    Productselect = c.fetchall()  # our tuple [(Aspergus, 2.3)]
     if len(Productselect) == 0:
         Currentquantity = 1
     else:
         quantity = Productselect[0][2]
         Currentquantity = quantity + 1
     AddToCart(productname, Currentquantity)
-    counterlist[i] = str(Currentquantity)  
+    counterlist[i] = str(Currentquantity)
 
-def  mincount(productname, i):
+
+def mincount(productname, i):
     sql1 = '''SELECT * FROM cart WHERE name=:c'''
     sql2 = {"c": productname}
     c.execute(sql1, sql2)
-    Productselect = c.fetchall() 
+    Productselect = c.fetchall()
     if len(Productselect) == 0:
         Currentquantity = 0
     else:
@@ -181,7 +198,8 @@ def  mincount(productname, i):
         Currentquantity = quantity - 1
         RemoveFromCart(productname, Currentquantity)
     counterlist[i] = str(Currentquantity)
-    
+
+
 def mbmincount(productname, i):
     sql1 = '''SELECT * FROM mbcounter WHERE name=:c'''
     sql2 = {"c": productname}
@@ -189,11 +207,12 @@ def mbmincount(productname, i):
     Mbselect = c.fetchall()
     if Mbselect[0][1] > 1:
         Currentquantity = Mbselect[0][1] - 1
-        mbcounterlist[i]= Currentquantity
+        mbcounterlist[i] = Currentquantity
         sql1 = '''UPDATE mbcounter SET amount  = '''
         sql2 = ''' WHERE name="'''
         sql3 = '''"'''
         c.execute(sql1 + str(Currentquantity) + sql2 + productname + sql3)
+
 
 def mbpluscount(productname, i):
     sql1 = '''SELECT * FROM mbcounter WHERE name=:c'''
@@ -201,116 +220,127 @@ def mbpluscount(productname, i):
     c.execute(sql1, sql2)
     Mbselect = c.fetchall()
     Currentquantity = Mbselect[0][1] + 1
-    mbcounterlist[i]= Currentquantity
+    mbcounterlist[i] = Currentquantity
     sql1 = '''UPDATE mbcounter SET amount  = '''
     sql2 = ''' WHERE name="'''
     sql3 = '''"'''
     c.execute(sql1 + str(Currentquantity) + sql2 + productname + sql3)
     print(mbcounterlist[i])
 
+
 @app.route('/pmb/')
 def pmb():
     productname = "MealboxVeganKap"
     mbpluscount(productname, 0)
-    return render_template("VeganKap.html", mbcounter = mbcounterlist)
+    return render_template("VeganKap.html", mbcounter=mbcounterlist)
+
 
 @app.route('/mmb/')
 def mmb():
     productname = "MealboxVeganKap"
     mbmincount(productname, 0)
-    return render_template("VeganKap.html", mbcounter = mbcounterlist)
+    return render_template("VeganKap.html", mbcounter=mbcounterlist)
+
 
 @app.route('/pwa/')
 def pwa():
     pluscount('White_Asparagus', 0)
-    return render_template("Product.html", counters = counterlist)
+    return render_template("Product.html", counters=counterlist)
+
+
 @app.route('/mwa/')
 def mwa():
     mincount('White_Asparagus', 0)
-    return render_template("Product.html", counters = counterlist)
+    return render_template("Product.html", counters=counterlist)
+
 
 @app.route('/pga/')
 def pga():
     pluscount('Green_Asparagus', 1)
-    return render_template("Product.html", counters = counterlist)
+    return render_template("Product.html", counters=counterlist)
+
+
 @app.route('/mga/')
 def mga():
     mincount('Green_Asparagus', 1)
-    return render_template("Product.html", counters = counterlist)
+    return render_template("Product.html", counters=counterlist)
+
 
 @app.route('/pol/')
 def pol():
-    pluscount('Organic_Leek', 2)
-    return render_template("Product.html", counters = counterlist)
+    pluscount('Chinese_Cabbage', 2)
+    return render_template("Product.html", counters=counterlist)
+
+
 @app.route('/mol/')
 def mol():
-    mincount('Organic_Leek', 2)
-    return render_template("Product.html", counters = counterlist)
+    mincount('Chinese_Cabbage', 2)
+    return render_template("Product.html", counters=counterlist)
+
 
 @app.route('/poc/')
 def poc():
-    pluscount('Organic_Carrots', 3)
-    return render_template("Product.html", counters = counterlist)
-@app.route('/moc/')
-def moc():
-    mincount('Organic_Carrots', 3)
-    return render_template("Product.html", counters = counterlist)
+    pluscount('Pak_Soi', 3)
+    return render_template("Product.html", counters=counterlist)
 
-@app.route('/por/')
+
+@ app.route('/moc/')
+def moc():
+    mincount('Pak_Soi', 3)
+    return render_template("Product.html", counters=counterlist)
+
+
+@ app.route('/por/')
 def por():
     pluscount('Organic_Radish', 4)
-    return render_template("Product.html", counters = counterlist)
-@app.route('/mor/')
+    return render_template("Product.html", counters=counterlist)
+
+
+@ app.route('/mor/')
 def mor():
     mincount('Organic_Radish', 4)
-    return render_template("Product.html", counters = counterlist)
+    return render_template("Product.html", counters=counterlist)
 
-@app.route('/pps/')
+
+@ app.route('/pps/')
 def pps():
-    pluscount('Pak_Soi', 5)
-    return render_template("Product.html", counters = counterlist)
-@app.route('/mps/')
+    pluscount('Organic_Carrots', 5)
+    return render_template("Product.html", counters=counterlist)
+
+
+@ app.route('/mps/')
 def mps():
-    mincount('Pak_Soi', 5)
-    return render_template("Product.html", counters = counterlist)
+    mincount('Organic_Carrots', 5)
+    return render_template("Product.html", counters=counterlist)
 
-@app.route('/pro/')
+
+@ app.route('/pro/')
 def pro():
-    pluscount('Red_Onions', 6)
-    return render_template("Product.html", counters = counterlist)
-@app.route('/mro/')
-def mro():
-    mincount('Red_Onions', 6)
-    return render_template("Product.html", counters = counterlist)
+    pluscount('Bananas', 6)
+    return render_template("Product.html", counters=counterlist)
 
-@app.route('/pcc/')
+
+@ app.route('/mro/')
+def mro():
+    mincount('Bananas', 6)
+    return render_template("Product.html", counters=counterlist)
+
+
+@ app.route('/pcc/')
 def pcc():
-    pluscount('Chinese_Cabbage', 7)
-    return render_template("Product.html", counters = counterlist)
-@app.route('/mcc/')
+    pluscount('Walnuts', 7)
+    return render_template("Product.html", counters=counterlist)
+
+
+@ app.route('/mcc/')
 def mcc():
-    mincount('Chinese_Cabbage', 7)
-    return render_template("Product.html", counters = counterlist)
+    mincount('Walnuts', 7)
+    return render_template("Product.html", counters=counterlist)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
     app.config['SERVER_NAME'] = "127.0.0.1:5000"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 connection.commit()
--- Sql.py
+++ Sql.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-#Tables: ingredients, user_status, user_password
+# Tables: ingredients, user_status, user_password
 
 def ReadDatabase():
     print("Tables:\ningredients\nuser_status\nuser_password\n\n")
@@ -14,20 +14,21 @@ def ReadDatabase():
 connection = sqlite3.connect("BitebyBite.db")
 c = connection.cursor()
 
-#Create the tables
+# Create the tables
 c.execute("create table ingredients (name text, price real)")
 c.execute("create table user_status (user text, status text)")
 c.execute("create table user_password (user text, password text)")
 c.execute("create table cart (name text, price real, quantity integer)")
 c.execute("create table mbcounter (name text, amount integer)")
 
-#Make the list to put in the corresponding table in the database
-list1 = [("MealboxVeganKap", 5.99),("White_Asparagus", 3.49),("Green_Asparagus", 3.49),("Organic_Leek", 0.69),("Organic_Carrots", 1.49),("Organic_Radish", 0.99),("Pak_Soi", 1.39),("Red_Onions", 0.89),("Chinese_Cabbage", 1.49)]
-list2 = [("Melvin", "logged_in"),("Nena", "logged_out")]
-list3 = [("Melvin", "admin"),("Nena","admin")]
+# Make the list to put in the corresponding table in the database
+list1 = [("MealboxVeganKap", 5.99), ("White_Asparagus", 3.49), ("Green_Asparagus", 3.49), ("Chinese_Cabbage", 1.49),
+         ("Pak_Soi", 1.39), ("Organic_Radish", 0.99), ("Organic_Carrots", 1.49), ("Bananas", 1.29), ("Walnuts", 2.49)]
+list2 = [("Melvin", "logged_in"), ("Nena", "logged_out")]
+list3 = [("Melvin", "admin"), ("Nena", "admin")]
 list4 = [("MealboxVeganKap", 1)]
 
-#populate the tables
+# populate the tables
 c.executemany("insert into ingredients values (?,?)", list1)
 c.executemany("insert into user_status values (?,?)", list2)
 c.executemany("insert into user_password values (?,?)", list3)
